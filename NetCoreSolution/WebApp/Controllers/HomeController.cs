c98b5b3ae0e008fb3596e61534ca443fc10457fd
cesarios <cesarios11@gmail.com>
2023-06-14 15:02:50 -0500

    Initial commit-Conociendo net Core

---
+++ NetCoreSolution/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Controlador Home método index";
+        }
+    }
+}
---
+++ NetCoreSolution/WebApp/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            //services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            /*
+            DefaultFilesOptions d = new DefaultFilesOptions();
+            d.DefaultFileNames.Clear();
+            d.DefaultFileNames.Add("noDefault.html");
+            app.UseDefaultFiles(d);
+            app.UseStaticFiles();
+            */
+
+            /*
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            */
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            
+            app.Use(async(context, next) => {
+                await context.Response.WriteAsync($"Entorno: {env.EnvironmentName}");
+                await next.Invoke();
+            });
+
+            app.Run(async (context) => {
+                //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                await context.Response.WriteAsync($"\n{_configuration["KeyWord"]}");
+            });
+            
+        }
+    }
+}
